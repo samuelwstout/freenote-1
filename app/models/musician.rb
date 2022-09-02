3add478888526ba394e5ef7773bb777e470ea3f7
samuelwstout <samuelwstout@gmail.com>
2022-09-01 23:39:00 -0500

    modify model relationships

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,7 +1,6 @@
 class Musician < ApplicationRecord
     has_many :job_applications
-    has_many :jobs, through: :job_applications
-    has_many :contractors, through: :jobs
+    has_many :jobs, through: :job_applications 
 
     has_secure_password
     validates :username, presence: true, uniqueness: true
