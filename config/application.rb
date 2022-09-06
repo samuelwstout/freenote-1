d582b591c7ad8945982fe5958e760afede8a3054
samuelwstout <samuelwstout@gmail.com>
2022-09-06 15:44:25 -0500

    Establish connection between User and subclasses

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Freenote
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += %W(#{config.root}/app/models/users)
   end
 end
