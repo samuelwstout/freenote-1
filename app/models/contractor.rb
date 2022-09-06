02e06e22c5bb736c252d735b9d106d9eab2e89d9
samuelwstout <samuelwstout@gmail.com>
2022-09-06 13:01:59 -0500

    migrate models

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,2 +1,6 @@
 class Contractor < ApplicationRecord
+    has_many :jobs
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,4 +1,7 @@
 class Musician < ApplicationRecord
     has_many :job_applications
     has_many :jobs, through: :job_applications
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
 end
