ddafa6c68af5e28dd70254d3ead9112496a031b1
samuelwstout <samuelwstout@gmail.com>
2022-09-06 15:35:13 -0500

    Create user

---
+++ db/migrate/20220906203401_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :location
+      t.string :instrument
+      t.text :bio
+      t.string :media_url
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
