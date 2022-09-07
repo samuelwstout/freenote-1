b3a449c85692d3b11283f8792a9016c3d8f085a7
samuelwstout <samuelwstout@gmail.com>
2022-09-07 09:49:48 -0500

    Create user and session controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
-
+    get "/me", to: "users#show"
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
